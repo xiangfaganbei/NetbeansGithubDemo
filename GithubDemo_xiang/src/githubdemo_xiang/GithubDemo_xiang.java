da3b37a76ecf8655fd0f0216ea08bdd583f1de03
fenghe <fenghebonjour@gmail.com>
2021-05-23 12:19:21 -0400

    new class say hi from  xiangfaganbei local

---
+++ GithubDemo_xiang/src/githubdemo_xiang/GithubDemo_xiang.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package githubdemo_xiang;
+
+/**
+ *
+ * @author fengh
+ */
+public class GithubDemo_xiang {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Hi from xiangfaganbei local 12:18 2021-05-23");
+    }
+    
+}
