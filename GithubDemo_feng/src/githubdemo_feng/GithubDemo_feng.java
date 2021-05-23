fdb1be2f9862bb36152b46b2c7c135b773b7f73a
fenghe <fenghebonjour@gmail.com>
2021-05-23 12:01:40 -0400

    say hello from fenghebonjour local

---
+++ GithubDemo_feng/src/githubdemo_feng/GithubDemo_feng.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package githubdemo_feng;
+
+/**
+ *
+ * @author fengh
+ */
+public class GithubDemo_feng {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Hello Github from fenghebonjour local 12:00 2021-05-23");
+    }
+    
+}
